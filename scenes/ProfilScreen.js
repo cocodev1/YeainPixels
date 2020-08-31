5e4833d5beddac44d25e649fb486f4cf96f80208
coco <simoncoco011@gmail.com>
2020-08-31 16:56:22 +0200

    Change style of StatScreen and ProfilScreen

--- scenes/ProfilScreen.js
+++ scenes/ProfilScreen.js
@@ -5,7 +5,7 @@ import { WHITE } from '../styles/colors'
 export default function ProfilScreen() {
     return(
         <View style={{justifyContent: 'center', alignItems: 'center', textAlign: 'center', bottom: 0, top: 0, left: 0, right: 0, position: 'absolute'}}>
-            <Text style={{fontSize: 35, color: WHITE, textAlign: 'center'}}>Comming soon</Text>
+            <Text style={{fontSize: 25, color: WHITE, textAlign: 'center'}}>Comming soon !</Text>
         </View>
     )
 }
--- scenes/StatScreen.js
+++ scenes/StatScreen.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import AvgMood from '../components/StatsScreen/AvgMood'
+import { View, Text } from 'react-native'
+import { WHITE } from '../styles/colors'
 
 export default function StatScreen() {
     return(
-        <AvgMood />
+        <View style={{flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <AvgMood />
+            <Text style={{color: WHITE, fontSize: 22}}>More Stats comming soon !</Text>
+        </View>
     )
 }
